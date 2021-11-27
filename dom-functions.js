a17652e097e5550bfbe2e514e9d08a3835dc8cba
Sheelove Dol <siesel26@gmail.com>
2021-11-27 13:02:39 -0800

    Create module to export function

---
+++ dom-functions.js
@@ -0,0 +1,13 @@
+// Module for DOM functions
+
+const toggleButton = element => {
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
+  }
+
+}
+
+// Syntax for exporting the individual functions you need from this module/file
+export { toggleButton }
--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const firstButton = document.getElementById('first');
 const hiddenButton = document.getElementById('hidden');
 
-const toggleButton = element => {
-  if (element.style.display === 'none') {
-    element.style.display = 'block';
-  } else {
-    element.style.display = 'none';
-  }
 
-}
 
 firstButton.addEventListener('click', () => {toggleButton(hiddenButton)})
 
